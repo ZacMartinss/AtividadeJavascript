d469d9682326357dea015b8da56357a93bfa7037
Isaac Martins <isaacmartins431@gmail.com>
2025-11-10 11:55:27 -0300

    Finalização das aulas 11 e 12 - Herança, Classes Abstratas e Interfaces

---
+++ aula11/Ciclista.ts
@@ -0,0 +1,19 @@
+import { Atleta } from "./Atleta";
+
+export class Ciclista extends Atleta {
+
+    constructor(nome: string) {
+        super(nome)
+    }
+
+    // IMPLEMENTAÇÃO DO MÉTODO ABSTRATO DA CLASSE MÃE
+    public aquecer(): void {
+        console.log("Fazendo alongamento...")
+    }
+
+    // MÉTODO PRÓPRIA DESSA CLASSE
+    public pedalar(): void {
+        console.log("Estou pedalando....")
+    }
+
+}
